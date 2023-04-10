79f486c44ff71c5ea07909b6d60d714e847f39f9
IsaacToluwani <toluwani.isaac@gmail.com>
2023-04-10 13:58:08 +0100

    practicing c

---
+++ personalCode/test2.c
@@ -0,0 +1,8 @@
+
+
+int integer_add (int x, int y)
+{
+	int result;
+	result = x + y;
+	return result;
+}
---
+++ personalCode/test3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main ()
+{
+	int card_name = 11;
+	if (card_name > 10)
+		printf ( "Yes, I got it");
+	card_name = card_name - 1;
+	return card_name;
+}
